ea656861dddd95abdc4cbfe684fe21a1ae08f3cc
Sarahcsy <1791086708@qq.com>
2018-12-22 10:56:56 +0800

    实验室设备管理系统

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <img src="beijing2.jpg" style=" width:1024px; height:768px;"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px; */
+  width:1024px; 
+  height:768px;
+}
+</style>
